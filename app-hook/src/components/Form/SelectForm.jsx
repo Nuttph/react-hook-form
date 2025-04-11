8a9b9ad0f06dc5b0d108ed1f56e1d45db5a645bd
Nuttaphon <nathn2420@gmail.com>
2025-04-11 08:02:05 +0700

    create form using MUI

--- app-hook/src/components/Form/SelectForm.jsx
+++ app-hook/src/components/Form/SelectForm.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link, Route, Routes, useLocation } from 'react-router-dom'
 import BasicForm from './BasicForm'
 import AdvanceForm from './AdvanceForm'
+import MyForm from './MyForm'
 
 const SelectForm = () => {
 
@@ -12,10 +13,12 @@ const SelectForm = () => {
         <div className='flex flex-row items-center justify-center'>
             <Link to={"/create/basic"} className={`${location.pathname == "/create/basic" ? "bg-yellow-500":""} border-r px-7`}>Basic form</Link>
             <Link to={"/create/advance"} className={`${location.pathname == "/create/advance" ? "bg-yellow-500":""} px-7`}>Advance form</Link>
+            <Link to={"/create/myform"} className={`${location.pathname == "/create/myform" ? "bg-yellow-500":""} px-7`}>MUI form</Link>
         </div>
         <Routes>
             <Route path='basic' element={<BasicForm/>}/>
             <Route path='advance' element={<AdvanceForm/>}/>
+            <Route path='myform' element={<MyForm/>}/>
         </Routes>
     </div>
   )
